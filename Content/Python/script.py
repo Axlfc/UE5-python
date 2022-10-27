e82399755d17dbfd3b79c427b1098ca55d6a75a1
Axel <axelfernandezcurros@gmail.com>
2022-10-27 21:05:31 +0200

    Update script.py

--- Content/Python/script.py
+++ Content/Python/script.py
@@ -3,6 +3,15 @@ import sys
 import socket
 import importlib
 
+classes = ["SoundWave", "Material", "MaterialInstanceConstant", "MaterialFunction", "Texture2D", "StaticMesh", "SkeletalMesh", "ObjectRedirector", "PhysicsAsset", "Skeleton", "UserDefinedEnum", "TextureRenderTarget2D", "Blueprint", "SoundCue", "WidgetBlueprint", "HapticFeedbackEffect_Curve", "MapBuildDataRegistry", "NiagaraParameterCollection", "NiagaraSystem"]
+
+
+EAL = unreal.EditorAssetLibrary
+# EUL = unreal.EditorUtilityLibrary
+EAS = unreal.EditorActorSubsystem
+
+world = unreal.World
+
 hostname = socket.gethostname()
 
 IPaddress = socket.gethostbyname(hostname)
@@ -37,14 +46,51 @@ def showPaths(gPaths):
 gamePaths = processPaths()
 
 
-def reload(text):
-    importlib.reload(text)
+def reload(scriptName):
+    print("Script " + str(scriptName) + " reloaded")
+    importlib.reload(scriptName)
+
+
+# Print paths of all assets under project root directory
+def processAssetPaths():
+    assets = []
+    gamePath = '/Game'
+    assetPaths = EAL.list_assets(gamePath)
+
+    for assetPath in assetPaths:
+        assets.append(assetPath)
+    return assets
+
+assetPaths = processAssetPaths()
+
+def getAllActors():
+    actors = []
+    allActors = unreal.get_editor_subsystem(EAS).get_all_level_actors()
+
+    for actor in allActors:
+        actors.append(actor)
+    return actors
+
+allActorsPath = getAllActors()
+
+
+def getAssetClass(classType):
+    assets = []
+    gamePath = '/Game'
+    assetPaths = EAL.list_assets(gamePath)
+
+    for assetPath in assetPaths:
+        assetData = EAL.find_asset_data(assetPath)
+        assetClass = assetData.asset_class
+        if classType == assetClass:
+            assets.append(assetData.get_asset())
+    return assets
+
+soundWaveActors = getAssetClass(classes[0])
 
 def main():
-
     #showPaths(gamePaths)
     pass
     
 
 main()
-
