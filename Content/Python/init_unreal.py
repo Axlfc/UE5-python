87fb193d282e5200d26af3b38278a14c60dc8ea8
Axel <axelfernandezcurros@gmail.com>
2022-10-30 22:32:27 +0100

    Update init_unreal.py

--- Content/Python/init_unreal.py
+++ Content/Python/init_unreal.py
@@ -1,1 +1,11 @@
 import script
+import importlib
+
+
+# Description: This function is used to reload the script
+def reload(scriptname):
+    print("Script " + str(scriptname) + " reloaded")
+    importlib.reload(scriptname)
+
+
+reload(script)
