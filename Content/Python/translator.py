6482280c5bef3f45635277d893fba086eef2426f
Axel <axelfernandezcurros@gmail.com>
2022-11-16 16:54:26 +0100

    Update translator.py

--- Content/Python/translator.py
+++ Content/Python/translator.py
@@ -7,8 +7,68 @@ from googletrans import Translator  # pip install googletrans==4.0.0-rc1
 # We need ffmpeg installed in the system (win: choco install ffmpeg / linux: sudo apt install ffmpeg)
 
 
+langs = {
+    "Amárico":  "am",
+    "Árabe":	"ar",
+    "Vasco":	"eu",
+    "Bengalí":	"bn",
+    "Inglés (Reino Unido)":	"en-GB",
+    "Portugués (Brasil)":	"pt-BR",
+    "Búlgaro":	"bg",
+    "Catalán":	"ca",
+    "Cheroqui":	"chr",
+    "Croata":	"h",
+    "Checo":	"cs",
+    "Danés":	"da",
+    "Neerlandés":	"nl",
+    "Inglés (EE.UU.)":	"en",
+    "Estonio":	"et",
+    "Filipino":	"fil",
+    "Finés":	"fi",
+    "Francés":	"fr",
+    "Alemán":	"de",
+    "Griego":	"el",
+    "Guyaratí":	"gu",
+    "Hebreo":	"iw",
+    "Hindi":	"hi",
+    "Húngaro":	"hu",
+    "Islandés":	"is",
+    "Indonesio":	"id",
+    "Italiano":	"it",
+    "Japonés":	"ja",
+    "Canarés":	"kn",
+    "Coreano":	"ko",
+    "Letón":	"lv",
+    "Lituano":	"lt",
+    "Malay":	"ms",
+    "Malabar":	"ml",
+    "Maratí":	"mr",
+    "Noruego":	"no",
+    "Polaco":	"pl",
+    "Portugués (Portugal)":	"pt-PT",
+    "Rumano":	"ro",
+    "Ruso":	"ru",
+    "Serbio":	"sr",
+    "chino (PRC)":	"zh-CN",
+    "Eslovaco":	"sk",
+    "Esloveno":	"sl",
+    "Español":	"es",
+    "Suajili":	"sw",
+    "Sueco":	"sv",
+    "Tamil":	"ta",
+    "Telugu":	"te",
+    "Tailandés":	"th",
+    "Chino (Taiwán)":	"zh-TW",
+    "Turco":	"tr",
+    "Urdu":	"ur",
+    "Ucraniano":	"uk",
+    "Vietnamita":	"vi",
+    "Galés":	"cy"
+}
+
+
 def audio_process(audiopath):
-    model = whisper.load_model("tiny")  # tiny, base, small, medium, large
+    model = whisper.load_model("medium")  # tiny, base, small, medium, large
 
     # detect the spoken language
     _, probs = model.detect_language(whisper.log_mel_spectrogram(whisper.pad_or_trim(whisper.load_audio(audiopath))).to(model.device))
