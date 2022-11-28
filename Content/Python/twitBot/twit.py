5990f9f70bbb6ebfb768f69eee5051ebfe92c5c5
Axlfc <axelfernandezcurros@gmail.com>
2022-11-28 21:03:17 +0100

    UPDATED: twit.py unlimited

--- Content/Python/twitBot/twit.py
+++ Content/Python/twitBot/twit.py
@@ -1,8 +1,11 @@
 from datetime import datetime
-import twint  # pip install twint
+import twint  # pip install --upgrade --user git+https://github.com/MarcosFP97/twint.git@origin/master#egg=twint
 import json
 import random
-# pip3 install --user --upgrade -e git+https://github.com/twintproject/twint.git@origin/master#egg=twint
+import nest_asyncio
+import os
+
+nest_asyncio.apply()
 
 # TODO: Do some web scrape to get some twitter handles
 url = "https://www.ign.com/wikis/game-companies-on-twitter/Longpages"
@@ -42,6 +45,7 @@ def get_user_num_tweets(user):
     c = twint.Config()
     c.Username = user
     c.Store_object = True
+    c.Hide_output = True
     twint.run.Lookup(c)
     user = twint.output.users_list[0]
     return user.tweets
@@ -51,22 +55,33 @@ def get_user_join_date(user):
     c = twint.Config()
     c.Username = user
     c.Store_object = True
+    c.Hide_output = True
     twint.run.Lookup(c)
     user = twint.output.users_list[0]
     return user.join_date
 
 
 def twitter(user, search=None, lang=None):
+    filename = user + ".csv"
     c = twint.Config()
     c.Username = user
-    c.Search = search
     c.Lang = lang
     c.Since = get_user_join_date(user)
     c.Until = datetime.today().strftime('%Y-%m-%d')
     c.Limit = get_user_num_tweets(user)
-    c.Store_object = True
-    twint.run.Search(c)
-    tweets = twint.output.tweets_list
+    c.Store_csv = True
+    c.Count = True
+    if os.path.exists(filename):
+        print('already present :' + user)
+    else:
+        c.Custom["tweet"] = ["tweet"]
+        c.Output = filename
+        c.Hide_output = True
+        c.Store_object = True
+        c.Search = search
+        twint.run.Search(c)
+        tweets = twint.output.tweets_list
+
     twits = []
 
     for tweet in tweets:
@@ -89,12 +104,14 @@ def get_hashtags(twits):
 
 
 def main():
-    # twits = twitter("katyperry")
+    accounts = ["sanchezcastejon", "katyperry"]
 
+    for account in accounts:
+        twits = twitter(account)
+        print(twits)
     '''hashtags = get_hashtags(twits)
     for hashtag in hashtags:
         print(hashtag)'''
-
     # print(get_tweet())
 
 
