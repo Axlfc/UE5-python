56e4041a9198de4858a2e1e8e5f6ef8db07ce5c5
Axel <axelfernandezcurros@gmail.com>
2022-12-03 00:48:36 +0100

    Update plot.py

--- Content/Python/chatBot/plot.py
+++ Content/Python/chatBot/plot.py
@@ -1,11 +1,31 @@
 import bot
+import re
 import os
 import sys
+import translator as translate
+
+
+def pythonCheck(text):
+    if re.search(r'^(for|while|if|def|try|except|else|elif|with|continue|break|#|from|import|return|pass|async|await|yield|raise|del|class|global|finally|assert)', text):
+        return True
+    # if it starts with a '(' then it's not python
+    if re.search(r'^\(', text):
+        return False
+     # if it starts or ends with a '=' then it's not python
+    if re.search(r'^=|=$', text):
+        return False
+    if re.search(r'\(|=', text):
+        return True
+    return False
 
 
 def main():
     repo_dir = os.path.join(os.getcwd().split("\n")[0], "voiceCloning")
+    lang = translate.detect(sys.argv[1])
     botanswer = bot.bot(sys.argv[1]).strip()
+    original_bot_answer = botanswer
+    if not lang == "en":
+        botanswer = translate.text_process(botanswer, "en")
     botanswernojumplines = botanswer.replace('\n', ' ')
     botvoice = "bella.wav"
 
@@ -14,7 +34,7 @@ def main():
     filename = repo_dir + "\\" + "outputs" + "\\" + botanswernojumplines[:20].replace(" ", "_").replace(",", "").replace(".", "").replace("'", "").replace(":", "") + ".wav"
     print()
     print()
-    print(botanswer)
+    print(original_bot_answer)
     voicecommand = "python playaudio.py " + filename
     os.system(voicecommand)
     os.remove(filename)
