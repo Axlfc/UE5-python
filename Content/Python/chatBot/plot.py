674703a079c02d030dbfddaa995e8f43bfb57f68
Axel <axelfernandezcurros@gmail.com>
2022-12-02 16:33:51 +0100

    Update plot.py

--- Content/Python/chatBot/plot.py
+++ Content/Python/chatBot/plot.py
@@ -1,35 +1,26 @@
-<<<<<<< HEAD
-import playaudio as play
 import bot
+import os
 import sys
-sys.path.append("..")
-import voice_cloning as voice
 
 
 def main():
-    botanswer = bot.bot(sys.argv[1])
-    botvoice = "C:\\Users\\AxelFC\\Desktop\\bella.wav"
+    repo_dir = os.path.join(os.getcwd().split("\n")[0], "voiceCloning")
+
+    botanswer = bot.bot(sys.argv[1]).strip()
+    botvoice = "bella.wav"
+
+    command = "python voice_cloning.py \"" + str(botanswer) + "\"" + " \"" + botvoice + "\""
+    os.system(command)
+    filename = repo_dir + "\\" + "outputs" + "\\" + botanswer[:20].replace(" ", "_").replace(",", "").replace(".", "").replace("'", "").replace(":", "") + ".wav"
+    print()
+    print()
     print(botanswer)
-    # TODO: Call to voice_cloning to output the bot response, play it and then remove the .wav file.
+    # TODO: Play audio file
+    voicecommand = "python playaudio.py " + filename
+    os.system(voicecommand)
+    # TODO: Remove audio file
+    os.remove(filename)
 
 
 if __name__ == '__main__':
     main()
-=======
-import playaudio as play
-import bot
-import sys
-sys.path.append("..")
-import voice_cloning as voice
-
-
-def main():
-    botanswer = bot.bot(sys.argv[1])
-    botvoice = "C:\\Users\\AxelFC\\Desktop\\bella.wav"
-    print(botanswer)
-    # TODO: Call to voice_cloning to output the bot response, play it and then remove the .wav file.
-
-
-if __name__ == '__main__':
-    main()
->>>>>>> 419ebd5635c3c7c96697dc245d9fde1d891043a7
