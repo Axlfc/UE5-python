47cd6b773b2895f07ce2befdcd8530d5aa2ccfb3
Axel <axelfernandezcurros@gmail.com>
2022-12-04 01:43:06 +0100

    Update conversate.py

--- Content/Python/chatBot/conversate.py
+++ Content/Python/chatBot/conversate.py
@@ -1,20 +1,38 @@
 import bot
 import colorama
 import os
+from datetime import datetime
 
 
-def save_log():
-    pass
+def add_message(message, initialtime):
+    now = datetime.now()
+    time = now.strftime("%H-%M-%S")
+    date = now.strftime("%m-%d-%Y")
+    repo_dir = os.path.join(os.getcwd().split("\n")[0], "conversations")
+    x = repo_dir + "\\" + date
+    filepath = x + "\\" + initialtime + ".txt"
+    if not os.path.exists(x):
+        os.mkdir(x)
+
+    with open(filepath, "a", encoding="utf-8") as f:
+        f.write(time.strip() + ": " + message.strip() + "\n")
+        # print("We should be adding the text: ", time + ":\t" + message + " to the file.")
 
 
 def conversate():
+    chat_log = []
+    initial_time = datetime.now().strftime("%m-%d-%Y_%H-%M-%S")
     while True:
         print("Enter your text:")
         text = input()
+        add_message(text, initial_time)
         if text == "exit" or text == "quit":
             exit(0)
-        print(colorama.Fore.YELLOW + bot.bot(text))
+        bot_answer = bot.bot(text)
+        add_message(bot_answer, initial_time)
+        print(colorama.Fore.YELLOW + bot_answer)
         print(colorama.Fore.RESET)
+        print(chat_log)
 
 
 def main():
