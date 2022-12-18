c8bda5dfdf266892bb00f4bbb5f2993ab69863e9
Axlfc <axelfernandezcurros@gmail.com>
2022-12-18 01:04:20 +0100

    UPDATED: Solving TODO to add audio as arguments on talk.py

--- Content/Python/chatBot/process_system.py
+++ Content/Python/chatBot/process_system.py
@@ -8,8 +8,6 @@ def process_pythonversion(platf):
     if platf == "Linux":
         # If customizer installation exists
         if os.path.isdir('~/.customizer/bin/chatGPT'):
-        # TODO: Detect if this is a Customizer installation and set python3 to the path in customizer instead of python3.
-            print("hey")
             return "~/.customizer/bin/chatGPT/bin/python3"
         else:
             return "python3"
