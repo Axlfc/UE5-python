f80d9bee4e56cf9d0c6573b163aa856980bb722d
Axlfc <axelfernandezcurros@gmail.com>
2023-03-20 13:36:22 +0100

    DELETED: GPT_J_6B.py and UPDATED: bot.py

--- Content/Python/chatBot/bot.py
+++ Content/Python/chatBot/bot.py
@@ -107,9 +107,13 @@ def bot(prompt, lang_model=last_openai_model):
         elif model == "FLAN-T5":
             import FLAN_T5
             return FLAN_T5.process_bot_answer(prompt)
-        elif model == "FLAN-UL2":
-            import FLAN_UL2
-            return FLAN_UL2.process_bot_answer(prompt)
+        elif model == "GALACTICA":
+            import GALACTICA
+            return GALACTICA.process_bot_answer(prompt)
+        elif model == "OPT":
+            import OPT
+            return OPT.process_bot_answer(prompt)
+
 
     else:
         show_error(model)
