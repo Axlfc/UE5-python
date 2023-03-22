ada9e508c8cecd1c96f5b054c09b899320abd1cd
Axlfc <axelfernandezcurros@gmail.com>
2023-03-22 11:57:39 +0100

    UPDATED: Leonardo AI

--- Content/Python/chatBot/leonardo_ai.py
+++ Content/Python/chatBot/leonardo_ai.py
@@ -22,11 +22,15 @@ def get_user_info():
     return response.text
 
 
-def generate_image(prompt, n=1, size_x=512, size_y=512):
+def generate_image(prompt, negative_prompt="", n=1, size_x=512, size_y=512):
     model_ids = {"Leonardo Creative": "6bef9f1b-29cb-40c7-b9df-32b51c1f67d3",
         "Leonardo Select": "cd2b2a15-9760-4174-a5ff-4d2925057376",
         "Leonardo Signature": "291be633-cb24-434f-898f-e662799936ad"
     }
+
+    preset_styles = ["NONE", "LEONARDO"]
+
+    stable_diffusion_versions = ["v1_5", "v2"]
 
     url = "https://cloud.leonardo.ai/api/rest/v1/generations"
     headers = {
@@ -40,12 +44,12 @@ def generate_image(prompt, n=1, size_x=512, size_y=512):
         "modelId": model_ids["Leonardo Creative"],
         "width": size_y,
         "height": size_x,
-        "negative_prompt": "ugly",
-        "sd_version": "v1_5",
-        "num_images": n,
+        "negative_prompt": negative_prompt,
+        "sd_version": stable_diffusion_versions[0],
+        "num_images": int(n),
         "num_inference_steps": 40,
         "guidance_scale": 7,
-        "presetStyle": "NONE",
+        "presetStyle": preset_styles[0],
         "tiling": False,
         "public": False
     }
@@ -80,16 +84,14 @@ def download_image(url, text):
 
 
 def main():
-    mask_filename = "mask.png"  # Replace with actual mask file name and path
-    # mask = Image.open(mask_filename).convert("RGBA")
-
     if len(sys.argv) < 2:
         print("Please provide a prompt or an image file name as the first argument.")
         print("Usage: python leonardo_ai.py \"<text_prompt>\"")
         return
 
     prompt = sys.argv[1]
-    n = int("1")
+    negative_prompt = sys.argv[2]
+    n = "1"
     size_x = 512
     size_y = 512
 
