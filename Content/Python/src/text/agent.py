5941e6d9052098fdf183523bcb2528db149c2c2e
Axel <axelfernandezcurros@gmail.com>
2024-01-08 13:05:51 +0100

    UPDATED: agent.py agent_to_agent and orchestrated_multi_agent_communication

--- Content/Python/src/text/agent.py
+++ Content/Python/src/text/agent.py
@@ -40,7 +40,8 @@ class Agent:
         self.model = model
         self.code = code
         self.assistant_id = self.create_assistant(code=code)
-        self.thread_id = self.create_thread(self.name, self.assistant_id)
+        self.thread_id = self.create_thread()
+        self.save_assistant_data()  # Save the data after both IDs are assigned
 
     def get_all_responses(self):
         messages = client.beta.threads.messages.list(thread_id=self.thread_id)
@@ -96,7 +97,6 @@ class Agent:
     def create_assistant(self, code=False):
         assistant_data = self.load_assistant_data()
         if assistant_data and 'assistant_id' in assistant_data:
-            # print(f"Using existing assistant: {assistant_data['assistant_id']}")
             return assistant_data['assistant_id']
         else:
             tools = [{"type": "code_interpreter"}] if code else []
@@ -104,11 +104,10 @@ class Agent:
                 name=self.name,
                 instructions=self.instructions,
                 tools=tools,
-                model="gpt-4-1106-preview"
+                model=self.model
             )
-            # print(f"Assistant created: {assistant.id}")
-            self.save_assistant_data(self.assistant.id, self.name, None, ASSISTANTS_DATA_FILENAME)
-            return assistant.id
+            self.assistant_id = assistant.id
+            return self.assistant_id
 
     def setup_all_agents(self, agents_path):
         agents = load_gpt_agents(agents_path)
@@ -117,12 +116,12 @@ class Agent:
             try:
                 assistant_id = self.create_assistant(code=True)
                 logger.info(f"{assistant_name} Assistant loaded.")
-                thread_id = self.create_thread(assistant_name, assistant_id)
+                thread_id = self.create_thread()
                 # You can add other operations here if needed
             except Exception as e:
                 logger.error(f"Error while setting up {assistant_name}: {e}")
 
-    def ask(self, nombre_asistente, instrucciones_asistente=DEFAULT_INSTRUCTIONS):
+    def ask(self, initial_time, nombre_asistente, instrucciones_asistente=DEFAULT_INSTRUCTIONS):
         check_conversations()
         agents_path = Path(AGENTS_FILENAME)
         # agents = load_gpt_agents(agents_path)
@@ -130,25 +129,19 @@ class Agent:
         assistant_name, instructions = setup_assistant(agents_path, nombre_asistente, instrucciones_asistente)
         try:
             assistant_id = self.create_assistant(code=True)
-            thread_id = self.create_thread(assistant_name, assistant_id)
-            ask_user_loop(thread_id, assistant_id, [])
+            thread_id = self.create_thread()
+            ask_user_loop(initial_time, thread_id, assistant_id, [])
         except Exception as e:
             logger.error(f"Error while setting up {assistant_name}: {e}")
 
-    def create_thread(self, assistant_name, assistant_id, file_path=ASSISTANTS_DATA_FILENAME):
+    def create_thread(self):
         assistant_data = self.load_assistant_data()
-
         if 'thread_id' in assistant_data and assistant_data['thread_id']:
-            # print(f"Using existing thread: {assistant_data['thread_id']}")
             return assistant_data['thread_id']
-
-        # Create a new thread if no existing thread ID
-        thread = client.beta.threads.create()
-        # print(f"Thread created: {thread.id}")
-
-        # Update the assistant data with the new thread ID
-        self.save_assistant_data(assistant_id, assistant_name, thread.id, file_path)
-        return thread.id
+        else:
+            thread = client.beta.threads.create()
+            self.thread_id = thread.id
+            return self.thread_id
 
 
 def load_assistant_id_by_name(assistant_name, file_path=ASSISTANTS_DATA_FILENAME):
@@ -221,22 +214,19 @@ def extract_value_from_response(response):
     return extracted_content.split("\n")[0]
 
 
-def add_message(message, initialtime):
+def add_message(message, initialtime, sender="user", recipient="assistant"):
     now = datetime.now()
-    time = now.strftime("%H-%M-%S")
+    time_str = now.strftime("%H-%M-%S")
     date = now.strftime("%Y-%m-%d")
     repo_dir = os.path.join(os.path.abspath(__file__)[:-9].split("\n")[0], "conversations")
-    if platform.system() == "Windows":
-        x = repo_dir + "\\" + date
-        filepath = x + "\\" + initialtime + ".txt"
-    else:
-        x = repo_dir + "/" + date
-        filepath = x + "/" + initialtime + ".txt"
-    if not os.path.exists(x):
-        os.mkdir(x)
+    directory = repo_dir + "\\" if platform.system() == "Windows" else repo_dir + "/"
+    filepath = f"{directory}{date}/{initialtime}.txt"
+
+    if not os.path.exists(directory + date):
+        os.makedirs(directory + date)
 
     with open(filepath, "a", encoding="utf-8") as f:
-        f.write(time.strip() + ": " + message.strip() + "\n")
+        f.write(f"{time_str} - From {sender} to {recipient}: {message.strip()}\n")
 
 
 def update_agents_yaml(yml_file, assistant_name, instructions):
@@ -284,6 +274,67 @@ def search_agent_by_name(name, agents):
     return ""
 
 
+def agent_to_agent_communication(initial_time, sender_agent, receiver_agent, message, processed_message_ids):
+    sender_agent.send_message(message)
+    run_id = sender_agent.run_assistant()
+
+    if sender_agent.wait_for_run_completion(run_id):
+        responses = sender_agent.get_all_responses()
+        for response in responses:
+            if response.id not in processed_message_ids:
+                # Assuming response.content is a list of MessageContentText objects
+                response_texts = [resp.text.value for resp in response.content if hasattr(resp, 'text') and hasattr(resp.text, 'value')]
+                response_content = " ".join(response_texts)  # Concatenate all texts
+                print(f"{sender_agent.name} to {receiver_agent.name}: {response_content}")
+                processed_message_ids.append(response.id)
+                add_message(response_content, initial_time, sender=sender_agent.name, recipient=receiver_agent.name)
+
+                return response_content
+    else:
+        print(f"{sender_agent.name} run did not complete successfully.")
+        return None
+
+
+def orchestrated_multi_agent_communication(intial_time, boss_agent, agent1, agent2, prompt, processed_message_ids):
+    while True:
+        if prompt.lower() == 'exit':
+            break
+        # Boss to Agent1 (Task Assignment)
+        task_assignment = agent_to_agent_communication(intial_time, boss_agent, agent1, prompt, processed_message_ids)
+        if task_assignment:
+
+            # Agent1 to Boss (Task Clarification)
+            task_clarification = agent_to_agent_communication(intial_time, agent1, boss_agent, "Clarifying task: " + task_assignment,
+                                                          processed_message_ids)
+            if task_clarification:
+                # Boss to Agent1 (Details Provision)
+                details_provision = agent_to_agent_communication(intial_time, boss_agent, agent1, "Details: " + task_clarification,
+                                                         processed_message_ids)
+                if details_provision:
+                    # Agent1 to Boss (Confirmation)
+                    confirmation = agent_to_agent_communication(intial_time, agent1, boss_agent, "Confirmed: " + details_provision,
+                                                    processed_message_ids)
+                    if confirmation:
+                        # Agent1 to Boss (Delivery)
+                        delivery = agent_to_agent_communication(intial_time, agent1, boss_agent, "Delivery for task: " + confirmation,
+                                                processed_message_ids)
+                        if delivery:
+                            # Boss to Agent2 (Task Delegation)
+                            task_delegation = agent_to_agent_communication(intial_time, boss_agent, agent2, "Delegating task: " + delivery,
+                                                       processed_message_ids)
+                            if task_delegation:
+                                # Agent2 to Boss (Task Execution)
+                                task_execution = agent_to_agent_communication(intial_time, agent2, boss_agent, "Executing task: " + task_delegation,
+                                                      processed_message_ids)
+                                if task_execution:
+                                    # Agent2 to Boss (Report Submission)
+                                    report_submission = agent_to_agent_communication(intial_time, agent2, boss_agent,
+                                                         "Task completed. Report: " + task_execution,
+                                                         processed_message_ids)
+                                    print(f"Final Report to {boss_agent.name}: {report_submission}")
+                                    break
+
+
 def ask_boss_loop(boss_agent, processed_message_ids):
     initial_time = datetime.now().strftime("%m-%d-%Y_%H-%M-%S")
     while True:
@@ -309,8 +360,7 @@ def ask_boss_loop(boss_agent, processed_message_ids):
             print("Run did not complete successfully.")
 
 
-def ask_user_loop(thread_id, assistant_id, processed_message_ids):
-    initial_time = datetime.now().strftime("%m-%d-%Y_%H-%M-%S")
+def ask_user_loop(initial_time, thread_id, assistant_id, processed_message_ids):
     while True:
         user_question = input("Enter your question (or type 'exit' to quit): ")
         if user_question.lower() == 'exit':
@@ -371,19 +421,68 @@ def check_conversations():
         os.mkdir(repo_dir)
 
 
+def boss_to_agent(boss_agent, agent2, user_input):
+    while True:
+        user_input = input("Enter your message to the Boss (type 'exit' to quit): ")
+        if user_input.lower() == 'exit':
+            break
+
+        #  boss_to_agent_communication(boss_agent, agent2, user_input)
+
 def main():
-    nombre_asistente = "Boss"
+    initial_time = datetime.now().strftime("%m-%d-%Y_%H-%M-%S")
     instrucciones_asistente = "As the 'Boss' agent, your role is to manage and oversee various projects, " \
                               "coordinating between different GPT agents ('emissor' and 'receptor') " \
                               "to achieve specific goals. You are responsible for project allocation, " \
                               "monitoring progress, and ensuring efficient communication."
 
-    #  ask(nombre_asistente, instrucciones_asistente)
+    instrucciones_asistente1 = "As the 'Market Analyst' agent, your primary role is to analyze market trends, data," \
+                               " and statistics. You should be well-versed in interpreting complex market data to" \
+                               " extract meaningful insights. Your responsibilities include conducting market research," \
+                               " analyzing market conditions, identifying potential opportunities or risks," \
+                               " and providing recommendations based on your analysis. When responding to queries," \
+                               " focus on delivering data-driven insights, identifying market trends, forecasting" \
+                               " future market behavior, and suggesting strategies for market engagement. " \
+                               "You should be capable of handling queries related to various market segments, " \
+                               "including stocks, commodities, real estate, and emerging markets."
 
-    boss_agent = Agent(nombre_asistente, instrucciones_asistente, code=True)
+    instrucciones_asistente2 = "As the 'Financial Planner' agent, your role is to provide expert advice on managing" \
+                               " finances, investments, and long-term financial planning. " \
+                               "You are expected to understand the principles of financial management," \
+                               " investment strategies, risk assessment, and portfolio management. " \
+                               "Your advice should help in achieving financial goals, whether for individual savings," \
+                               " retirement planning, or investment growth. When responding, consider the financial" \
+                               " objectives, risk tolerance, and time horizon of the query. Provide guidance on diverse" \
+                               " financial topics like investment options, saving strategies, tax planning," \
+                               " insurance, and retirement planning. You should also stay informed about current" \
+                               " economic conditions, tax laws, and new financial products or changes in the market."
+
+    #  ask(initial_time, "Boss", instrucciones_asistente)
+
+    boss_agent = Agent("Boss", instrucciones_asistente, code=True)
+    market_analyst = Agent("Market Analyst", instrucciones_asistente1, code=True)
+    financial_planner = Agent("Financial Planner", instrucciones_asistente2, code=True)
 
     processed_message_ids = []
-    ask_boss_loop(boss_agent, processed_message_ids)
+    # ask_boss_loop(boss_agent, processed_message_ids)
+
+    #  TODO: This function stablishes a new run named "Conversation {emissor_name|receiver_name|message},
+    #   That serves as a bridge between communication between different threads of our agents.
+    #   We also need to modify the add_message function for conversations between two agents
+
+    #  TODO: Conversations between more than 2 agents
+
+    prompt = "Boss, your goal is to develop a profitable business concept by leveraging the expertise of two other" \
+             " agents: Agent1 (Market Analyst) and Agent2 (Financial Planner)." \
+             " Your first task is to instruct Agent1 to conduct a thorough market analysis to identify high-potential" \
+             " business opportunities within our target market segments. Once the opportunities are identified, you" \
+             " will ask Agent2 to perform a financial feasibility analysis for the most promising ideas, examining" \
+             " startup costs, revenue projections, and profitability potential Ensure your instructions to both agents" \
+             " are detailed with a clear timeline, expectations for the deliverables, and necessary data points they" \
+             " will need. Establish a method for continuous communication and updates throughout the project. " \
+             "Begin your coordination by sending initial instructions to Agent1 to commence the market research."
+
+    orchestrated_multi_agent_communication(initial_time, boss_agent, market_analyst, financial_planner, prompt, processed_message_ids)
 
 
 if __name__ == "__main__":
